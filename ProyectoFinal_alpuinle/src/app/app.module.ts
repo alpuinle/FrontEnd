ca956e219cbde9ae81c6ce7e12f1d577e683443e
alpuinle <leoalpuin21@hotmail.com>
2023-05-08 18:05:08 -0300

    Creación componente "Acerca de"
    Creación componente y corrección de errores en Bootstrap

--- ProyectoFinal_alpuinle/src/app/app.module.ts
+++ ProyectoFinal_alpuinle/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoArgProgComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
